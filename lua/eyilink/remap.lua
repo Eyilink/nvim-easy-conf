750a6e842b1a6e749fcefd458b5c1f982e09cc8d
Eyilink Eyilink <eyilink@outlook.com>
2025-05-25 17:32:58 +0200

    auto pairing

--- lua/eyilink/remap.lua
+++ lua/eyilink/remap.lua
@@ -1,2 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>	", vim.cmd.Ex)
+vim.opt.number = true
+
+-- Auto-pairs mappings
+vim.keymap.set('i', '(', '()<Left>', { noremap = true })
+vim.keymap.set('i', '[', '[]<Left>', { noremap = true })
+vim.keymap.set('i', '"', '""<Left>', { noremap = true })
+vim.keymap.set('i', "'", "''<Left>", { noremap = true })
+vim.keymap.set('i', '`', '``<Left>', { noremap = true })
+-- Auto-format braces with newlines
+vim.keymap.set('i', '{<CR>', '{<CR>}<Esc>O', { noremap = true })
+vim.keymap.set('i', '{;<CR>', '{<CR>};<Esc>O', { noremap = true })
