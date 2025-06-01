3ed756dc20a437b0dde5050c3ffab71a57f2b66f
Eyilink Eyilink <eyilink@outlook.com>
2025-06-01 21:37:01 +0200

    adding vim options

--- lua/eyilink/remap.lua
+++ lua/eyilink/remap.lua
@@ -1,6 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>	", vim.cmd.Ex)
 vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabshop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
 
 -- Auto-pairs mappings
 vim.keymap.set('i', '(', '()<Left>', { noremap = true })
@@ -11,3 +19,5 @@ vim.keymap.set('i', '`', '``<Left>', { noremap = true })
 -- Auto-format braces with newlines
 vim.keymap.set('i', '{<CR>', '{<CR>}<Esc>O', { noremap = true })
 vim.keymap.set('i', '{;<CR>', '{<CR>};<Esc>O', { noremap = true })
+
+
