1d15558f577a6ea16b9cfd620185633b879ffebc
Eyilink <38373463+Eyilink@users.noreply.github.com>
2025-06-14 14:28:08 +0200

    Update init.lua

--- lua/eyilink/init.lua
+++ lua/eyilink/init.lua
@@ -1,2 +1,7 @@
 require("eyilink.remap")
 print("hello from eyilink")
+
+-- In your init.lua
+vim.cmd('silent! source ~/.config/nvim/lua/eyilink/packer.lua')
+vim.cmd('silent! PackerSync')
+vim.cmd('silent! PackerSync')
