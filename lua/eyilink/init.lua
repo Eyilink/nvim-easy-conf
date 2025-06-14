82406d9b3eb5358a42e253197f76d0c3dd3c1b74
Eyilink <38373463+Eyilink@users.noreply.github.com>
2025-06-14 14:55:49 +0200

    Update init.lua

--- lua/eyilink/init.lua
+++ lua/eyilink/init.lua
@@ -1,11 +1,7 @@
 require("eyilink.remap")
 print("hello from eyilink")
 
-local packer_path = '~/.config/nvim/lua/eyilink/packer.lua'
-if vim.fn.filereadable(packer_path) == 1 then
-    vim.cmd('source ' .. packer_path)
-    if pcall(require, 'packer') then
-        vim.cmd('PackerSync')
-        vim.cmd('PackerSync')
-    end
-end
+-- In your init.lua
+vim.cmd('source /home/exefree/.config/nvim/lua/eyilink/packer.lua')
+vim.cmd('PackerSync')
+vim.cmd('PackerSync')
