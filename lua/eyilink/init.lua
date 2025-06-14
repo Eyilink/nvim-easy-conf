fa52b35289fccc36d8674a37c72f83ae5124b610
Eyilink <38373463+Eyilink@users.noreply.github.com>
2025-06-14 15:16:29 +0200

    Update init.lua

--- lua/eyilink/init.lua
+++ lua/eyilink/init.lua
@@ -2,6 +2,13 @@ require("eyilink.remap")
 print("hello from eyilink")
 
 -- In your init.lua
-vim.cmd('source /home/exefree/.config/nvim/lua/eyilink/packer.lua')
-vim.cmd('PackerSync')
-vim.cmd('PackerSync')
+local flag_file = vim.fn.stdpath('data') .. '/packer_synced'
+
+if vim.fn.filereadable(flag_file) == 0 then
+    vim.cmd('source /home/exefree/.config/nvim/lua/eyilink/packer.lua')
+    vim.cmd('PackerSync')
+    vim.cmd('PackerSync')
+    
+    -- Create the flag file
+    vim.fn.writefile({}, flag_file)
+end
