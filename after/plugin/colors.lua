02d5bd4ad1387464e9cc8d42904cdc0349f64ca4
Eyilink <38373463+Eyilink@users.noreply.github.com>
2026-09-03 00:26:45 +0200

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,16 +6,6 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })	
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-	vim.api.nvim_set_hl(0, "StatusLine", {
-	    fg = "#908caa",
-	    bg = "none",
-	})
-
-	vim.api.nvim_set_hl(0, "StatusLineNC", {
-	    fg = "#6e6a86",
-	    bg = "none",
-	})
-
 end
 
 ColorMyPencils()
