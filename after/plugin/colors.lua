50fc9d6beaf307bc6a07926ce20baf3de660886b
Eyilink <38373463+Eyilink@users.noreply.github.com>
2026-09-02 22:44:38 +0200

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,20 @@
 
 function ColorMyPencils(color)
-	-- color = color or "rose-pine"
-	-- vim.cmd.colorscheme(color)
+	color = "rose-pine"
+	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })	
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+	vim.api.nvim_set_hl(0, "StatusLine", {
+	    fg = "#908caa",
+	    bg = "none",
+	})
+
+	vim.api.nvim_set_hl(0, "StatusLineNC", {
+	    fg = "#6e6a86",
+	    bg = "none",
+	})
 
 end
 
