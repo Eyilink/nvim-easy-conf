92e01f2c1117ae9fb5654b54293e865a400abaa3
Eyilink <38373463+Eyilink@users.noreply.github.com>
2026-09-09 18:01:18 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,22 @@
 require("eyilink")
-print("hello")
+--print("hello")
+
+-- Guardar el primer argumento (directorio) al iniciar Neovim
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    -- Verificar si hay argumentos y si el primer argumento es un directorio
+    if vim.fn.argc() > 0 then
+      local first_arg = vim.fn.argv(0)
+      if vim.fn.isdirectory(first_arg) == 1 then
+        vim.g.start_dir = first_arg
+      else
+        -- Si el primer argumento no es un directorio, usar el directorio actual
+        vim.g.start_dir = vim.fn.getcwd()
+      end
+    else
+      -- Si no hay argumentos, usar el directorio actual
+      vim.g.start_dir = vim.fn.getcwd()
+    end
+  end,
+})
